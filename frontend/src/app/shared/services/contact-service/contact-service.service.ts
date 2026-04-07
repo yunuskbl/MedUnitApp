7a4db118eded81329f28ef04f18951b714b9fa16
yunuskbl <yunuskoball12@gmail.com>
2026-04-07 18:21:18 +0300

    18:'1

--- frontend/src/app/shared/services/contact-service/contact-service.service.ts
+++ frontend/src/app/shared/services/contact-service/contact-service.service.ts
@@ -12,7 +12,7 @@ export interface ContactPayload {
 
 @Injectable({ providedIn: 'root' })
 export class ContactService {
-  private apiUrl = 'https://medunitapp.onrender.com/api/contact';
+  private apiUrl = 'https://medunit.vercel.app/contact';
 
   constructor(private http: HttpClient) {}
 
